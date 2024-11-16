4fd61a80fc984dbe92b23c0c71a635e707e5a05a
Mubashir P A <mubashirpa2002@gmail.com>
2024-11-16 16:59:27 +0530

    Add Queue Data Structure

---
+++ src/main/kotlin/queue/QueueRunner.kt
@@ -0,0 +1,14 @@
+package queue
+
+fun main() {
+    val queue =
+        QueueImpl<String>().apply {
+            enqueue("Ray")
+            enqueue("Brian")
+            enqueue("Eric")
+        }
+    println(queue)
+    queue.dequeue()
+    println(queue)
+    println("Next up: ${queue.peek()}")
+}
